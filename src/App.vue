fc7e546a019de6aaae10a7f94583926573df98b7
claudiopatti <claudiopatti1997@gmail.com>
2024-09-12 19:41:47 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script>
+// 1) Importazione del componente 
+import AppTitle from './components/AppTitle.vue'
+
+  export default {
+  data() {
+    return { 
+        message: 'La mia prima app con Vite!'
+    }
+  },
+  // 2) Dichiarizone del componente
+  components: {
+    AppTitle
+  },
+}
+</script>
+
+<template>
+    <!-- Utilizzo del componente  -->
+     <AppTitle />
+  <div class="tittle">
+    <h2>
+      {{ message }}
+    </h2>
+  </div>
+
+</template>
+
+<style scoped>
+.tittle {
+  text-align: center;
+}
+
+.tittle > h2 {
+  color: lightcoral;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,30 @@
+<script>
+  export default {
+  data() {
+    return { 
+        message: 'Template Vite + Vue'
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+    <h1>
+      {{ message }}
+    </h1>
+  </header>
+
+</template>
+
+<style scoped>
+hEADER {
+  background-color: aqua;
+  text-align: center;
+  padding: 15px 0;
+}
+
+h1 {
+  color: blue;
+}
+</style>
